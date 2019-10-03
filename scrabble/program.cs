f5dd2d69f530798de7c43bf8a08775c129f5772a
Andrew PETerson and Kira Roberts <kira@>
2019-10-03 11:25:56 -0700

    add front end logic

--- scrabble/program.cs
+++ scrabble/program.cs
@@ -7,7 +7,9 @@ namespace Scrabble
     {
         public static void Main()
         {
-            
+            Console.WriteLine("Enter a word:");
+            string input = Console.ReadLine();
+            Console.WriteLine(Game.CheckCharPoints(input));
         }
     }
 }
